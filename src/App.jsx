574c2f0fa26d7195c87fe1cad1315c6bcc642def
dev-guenay <guenay.a.k@outlook.de>
2026-03-25 18:14:56 +0100

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl text-blue-500">Tailwind läuft 🔥</h1>
+      <div className="bg-red-500 h-screen">
+  <h1 className="text-white text-5xl">TEST</h1>
+</div>
+    </div>
+    
+  );
+}
+
+export default App;
